2d5f8a88fed77e2c83cf6a801a03b06be07cad04
DANIELKAKAI <danielkakai3@gmail.com>
2024-07-01 20:02:44 +0300

    contract update

--- packages/react-app/components/Lockfunds.tsx
+++ packages/react-app/components/Lockfunds.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { createPublicClient, createWalletClient, custom, http } from "viem";
 import { celoAlfajores } from "viem/chains";
+import { v4 as uuidv4 } from "uuid";
 import { useAccount } from "wagmi";
 import { letapayContractAddress, cUsdToWei, cUsdAddress } from "../utils/utils";
 const LetapayABIJson = require("../../../../hardhat/artifacts/contracts/LetapayV2.sol/LetapayV2.json");
 const ERC20ABIJson = require("../../../../hardhat/utils/erc20.abi.json");
 
-function Payments({ postPayment, payments, setPayments, userAddress }) {
-  const [recieverAddressInput, setRecieverAddressInput] = useState("");
+function Lockfunds({ postPayment, payments, setPayments, userAddress }) {
   const [amountInput, setAmountInput] = useState("");
   const [dateInput, setDateInput] = useState("");
   const [timeInput, setTimeInput] = useState("");
@@ -22,13 +22,14 @@ function Payments({ postPayment, payments, setPayments, userAddress }) {
   });
 
   const handleAddClick = async () => {
-    if (recieverAddressInput && dateInput && timeInput && amountInput) {
+    if (dateInput && timeInput && amountInput) {
       if (typeof window.ethereum !== "undefined") {
         const paymentId = await postPayment(
-          recieverAddressInput,
+          userAddress,
           dateInput,
           timeInput,
-          amountInput
+          amountInput,
+          "LOCKED"
         );
 
         const approveTx = await walletClient.writeContract({
@@ -50,7 +51,7 @@ function Payments({ postPayment, payments, setPayments, userAddress }) {
           abi: LetapayABIJson.abi,
           functionName: "addPayment",
           account: userAddress,
-          args: [paymentId, recieverAddressInput, cUsdToWei(amountInput)],
+          args: [paymentId, userAddress, cUsdToWei(amountInput)],
         });
 
         let receipt = await publicClient.waitForTransactionReceipt({
@@ -61,14 +62,13 @@ function Payments({ postPayment, payments, setPayments, userAddress }) {
 
         alert(`${paymentId} added`);
 
-        setRecieverAddressInput("");
         setDateInput("");
         setTimeInput("");
         setAmountInput("");
 
         setPayments([
           {
-            receiver_address: recieverAddressInput,
+            receiver_address: userAddress,
             payment_datetime: `${dateInput}T${timeInput}`,
             amount: amountInput,
             status: "INPROGRESS",
@@ -84,15 +84,6 @@ function Payments({ postPayment, payments, setPayments, userAddress }) {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Letapay</h1>
-        <div>
-          <input
-            type="text"
-            value={recieverAddressInput}
-            onChange={(e) => setRecieverAddressInput(e.target.value)}
-            placeholder="Enter address"
-          />
-        </div>
         <div>
           <input
             type="number"
@@ -116,23 +107,21 @@ function Payments({ postPayment, payments, setPayments, userAddress }) {
           />
         </div>
         <button className="add-button" onClick={handleAddClick}>
-          Add
+          Lock
         </button>
         <table>
           <thead>
             <tr>
-              <th>Reciever Address</th>
               <th>Amount</th>
-              <th>Payment Date Time</th>
+              <th>Release Date Time</th>
               <th>Status</th>
             </tr>
           </thead>
           <tbody>
             {payments.map((payment, index) => {
-              if (payment.payment_type === "SCHEDULED") {
+              if (payment.payment_type === "LOCKED") {
                 return (
                   <tr key={index}>
-                    <td>{payment.receiver_address}</td>
                     <td>{payment.amount}</td>
                     <td>{payment.payment_datetime}</td>
                     <td>{payment.status}</td>
@@ -148,4 +137,4 @@ function Payments({ postPayment, payments, setPayments, userAddress }) {
   );
 }
 
-export default Payments;
+export default Lockfunds;
--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -1,40 +1,109 @@
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import Payments from "@/components/Payments";
+import Lockfunds from "@/components/Lockfunds";
+
+const url = "https://a0d2-102-217-172-2.ngrok-free.app";
 
 export default function Home() {
-    const [userAddress, setUserAddress] = useState("");
-    const [isMounted, setIsMounted] = useState(false);
-    const { address, isConnected } = useAccount();
+  const [userAddress, setUserAddress] = useState("");
+  const [isMounted, setIsMounted] = useState(false);
+  const [payments, setPayments] = useState([]);
+  const { address, isConnected } = useAccount();
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+  const postPayment = async (
+    receiverAddressInput,
+    dateInput,
+    timeInput,
+    amountInput,
+    paymentType = "SCHEDULED"
+  ) => {
+    try {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
 
-    useEffect(() => {
-        if (isConnected && address) {
-            setUserAddress(address);
-        }
-    }, [address, isConnected]);
+      const raw = JSON.stringify({
+        sender_address: userAddress,
+        receiver_address: receiverAddressInput,
+        amount: amountInput,
+        payment_datetime: `${dateInput}T${timeInput}:00+03:00`,
+        status: "INPROGRESS",
+        payment_type: paymentType,
+      });
 
-    if (!isMounted) {
-        return null;
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      const response = await fetch(
+        `${url}/payments`,
+        requestOptions
+      );
+      const result = await response.json();
+      console.log(result);
+
+      if (response.ok) {
+        return result.payment_id;
+      } else {
+        throw new Error(result.message || "Something went wrong");
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
+  };
 
-    return (
-        <div className="flex flex-col justify-center items-center">
-            {isConnected ? (
-                <>
-                <div className="h1 text-center">
-                    Your address: {userAddress}
-                </div>
-                <div>
-                    <Payments/>
-                </div>
-                </>
-            ) : (
-                <div>No Wallet Connected</div>
-            )}
-        </div>
-    );
+  const getPayments = async (senderAddress) => {
+    try {
+      const requestOptions = {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+          },
+        redirect: "follow",
+      };
+
+      fetch(
+        `${url}/payments?sender_address=${senderAddress}`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => setPayments(result))
+        .catch((error) => console.error(error));
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    setIsMounted(true);
+    if (isConnected && address) {
+      setUserAddress(address);
+      getPayments(address);
+    }
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      {userAddress ? (
+        <>
+          <div className="h1 text-center">Your address: {userAddress}</div>
+          <div>
+            <Payments postPayment={postPayment} payments={payments} setPayments={setPayments} userAddress={userAddress} />
+            <Lockfunds postPayment={postPayment} payments={payments} setPayments={setPayments} userAddress={userAddress} />
+          </div>
+        </>
+      ) : (
+        <div>No Wallet Connected</div>
+      )}
+    </div>
+  );
 }
